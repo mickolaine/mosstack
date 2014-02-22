93836c4c3c4cf2ddabc42eec5d8987ef44ae81db
mikko_laine <mikko.laine@gmail.com>
2014-02-22 14:42:27 +0200

    Version 0.1.0

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ else:
 '''
 setup(
     name='pyAstroStack',
-    version='0.1',
+    version='0.1.0',
     author='Mikko Laine',
     author_email='mikko.laine@gmail.com',
     packages=['pyastrostack', 'pyastrostack.Demosaic', 'pyastrostack.Registering', 'pyastrostack.Stacker'],
