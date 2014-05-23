af9d11463050ade55d44a7e9c2f38bed3bd07835
Mikko Laine <mikko.laine@gmail.com>
2014-05-23 14:48:40 +0300

    0.4.0 final

--- setup.py
+++ setup.py
@@ -4,30 +4,9 @@ from distutils.extension import Extension
 from Cython.Build import cythonize
 import numpy
 
-'''
-try:
-    from Cython.Distutils import build_ext
-except ImportError:
-    use_cython = False
-else:
-    use_cython = True
-
-cmdclass = { }
-ext_modules = [ ]
-
-if use_cython:
-    ext_modules += [
-        Extension("pyastrostack.Registering._step2", ["pyastrostack/Registering/_step2.pyx"]),
-    ]
-    cmdclass.update({ 'build_ext': build_ext })
-else:
-    ext_modules += [
-        Extension("pyastrostack.Registering._step2", ["pyastrostack/Registering/_step2.c"]),
-    ]
-'''
 setup(
     name='pyAstroStack',
-    version='0.4dev',
+    version='0.4.0',
     author='Mikko Laine',
     author_email='mikko.laine@gmail.com',
     packages=['pyastrostack', 'pyastrostack.Debayer', 'pyastrostack.Registering', 'pyastrostack.Stacker'],
