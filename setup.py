64e27472594f7197892da5ea7415095637e72002
mikko_laine <mikko.laine@gmail.com>
2014-12-25 15:51:51 +0200

    Changing distutils to setuptools. Renaming continues

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
-# from setuptools import setup, find_packages
-from distutils.core import setup
-from distutils.extension import Extension
+from setuptools import setup, find_packages
+# from distutils.core import setup
+# from distutils.extension import Extension
 from Cython.Build import cythonize
 import numpy
 
@@ -13,16 +13,16 @@ setup(
 >>>>>>> Renaming astrostack to mosstack begun
     author='Mikko Laine',
     author_email='mikko.laine@gmail.com',
-    packages=['mosstack', 'mosstack.Debayer', 'mosstack.Registering', 'mosstack.Stacker'],
-    #    packages=find_packages(),
+    # packages=['mosstack', 'mosstack.Debayer', 'mosstack.Registering', 'mosstack.Stacker'],
+    packages=find_packages(),
     scripts=['scripts/mosstack', 'scripts/mosstackgui'],
     include_dirs=[numpy.get_include()],
     #   cmdclass = cmdclass,
     #    ext_modules=ext_modules,
-    ext_modules = cythonize(["mosstack/Registering/_step2.pyx",
-                             "mosstack/Debayer/_BilinearCython.pyx",
-                             "mosstack/Debayer/_VNGCython.pyx",
-                             "mosstack/Stacker/_math.pyx"]),
+    ext_modules=cythonize(["mosstack/Registering/_step2.pyx",
+                           "mosstack/Debayer/_BilinearCython.pyx",
+                           "mosstack/Debayer/_VNGCython.pyx",
+                           "mosstack/Stacker/_math.pyx"]),
     data_files=[
         ("share/mosstack/", ["data/mosstack.xpm", "doc/LaTeX/manual.pdf"]),
         ("share/applications/", ["data/mosstack.desktop"])
@@ -31,10 +31,11 @@ setup(
     license='LICENSE.txt',
     description='Stacking software for astronomical images',
     long_description=open('README.txt').read(),
-    #    install_requires=[
-    #        "Pillow >= 2.2.1",
-    #        "NumPy >= 1.6.0",
-    #        "pyopencl >= 2013",
-    #        "astropy >= 0.2.4",
-    #    ],
-    )
+    install_requires=[
+        "Pillow >= 2.2.1",
+        "NumPy >= 1.6.0",
+        "pyopencl >= 2013",
+        "astropy >= 0.2.4",
+        "Cython >= 0.19",
+    ],
+)
